fd6506272361e22ba16425f2476bdda1225531cc
labatete-johnkenneth <katczinsky244@gmail.com>
2025-03-15 09:42:34 +0800

    Finished

--- script - mobile.js
+++ script - mobile.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
       h1: "Touch Me Not",
       h4: "(Noli Me Tángere)",
       p: "In more than a century since its appearance, José Riza's Noli Me Tangere has become widely known as the great novel of the Philippines. A passionate love story set against the ugly political backdrop of repression, torture, and murder, \"The Noli,\" as it is called in the Philippines, was the first major artistic manifestation of Asian resistance to European colonialism, and Rizal became a guiding conscience and martyr for the revolution that would subsequently rise up in the Spanish province.",
-      buttonText: "Read Now",
+      buttonText: "Download",
       buttonLink: "Books/Touch Me Not by José Rizal.epub"
     },
     {
       h1: "Subversion",
       h4: "(El Filibusterismo)",
       p: "El Filibusterismo (The Subversive) is the second novel by Jose Rizal (1861-1896), national hero of the Philippines. Like its predecessor, the better-known Noli Me Tangere, the Fili was written in Castilian while Rizal was traveling and studying in Europe. It was published in Ghent in 1891 and later translated into English, German, French, Japanese, Tagalog, Ilonggo, and other languages. A nationalist novel by an author who has been called the first Filipino, its nature as a social document of the late-nineteenth-century Philippines is often emphasized. For many years, copies of the Fili were smuggled into the Philippines after it was condemned as subversive by the Spanish authorities. Characters from the Noli (Basilio, Dona Victorina, Padre Salvi) return while new ones are introduced: Simoun, the transformed Ibarra; Cabesang Tales and his struggle for justice; the nationalist student Isagani; the Indio priest Padre Florentino. Through them the colonial milieu is expanded - its officialdom, education, legal system, power plays, social patterns - and seen anew as context for conflict and insight. editions of the original manuscripts. The result is the most authoritative and faithful English translation to date, one which attempts to preserve in English the cadence and color of the original.(Subversion)",
-      buttonText: "Read Now",
+      buttonText: "Download",
       buttonLink: "Books/Subversion by José Rizal.epub"
     },
   ];
